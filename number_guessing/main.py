5f724f316432671e7ad64aa7182558aacdc50c12
ArshadAzaad3299 <arshad.2017094@iit.ac.lk>
2020-06-23 01:02:00 +0530

    v1

---
+++ number_guessing/main.py
@@ -0,0 +1,15 @@
+#################################
+# NUMBER GUESSING GAME
+#################################
+
+import random
+
+print('***************************************************')
+print("NUMBER GUESSING GAME")
+print('***************************************************')
+print("The System randomly picks a number from 1 - 50 ")
+print("User should guess the number")
+
+value = random.randint(0, 10)
+
+print(value)
