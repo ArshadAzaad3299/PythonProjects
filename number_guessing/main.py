7c2f0db7386ee49879e076f4f465c1c2c961d16a
ArshadAzaad3299 <arshad.2017094@iit.ac.lk>
2020-06-23 01:59:15 +0530

    number_guessing done

--- number_guessing/main.py
+++ number_guessing/main.py
@@ -10,6 +10,15 @@ print('***************************************************')
 print("The System randomly picks a number from 1 - 50 ")
 print("User should guess the number")
 
-value = random.randint(0, 10)
+systemValue = random.randint(0, 10)
+runProgram = True
 
-print(value)
+while(runProgram):
+    userValue = int(input("Enter Your Guess: "))
+    if userValue == systemValue:
+        print("Correct Answer")
+        runProgram = False
+    elif userValue > systemValue:
+        print("your value is greater than System value")
+    elif userValue < systemValue:
+        print("your value is smaller than System value")
